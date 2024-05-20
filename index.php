9913c58e0daadb33926666c0b7de2df673a97b3c
Rhass. <aguilarrhazzhamir@gmail.com>
2024-05-20 22:36:50 +0800

    Now

--- index.php
+++ index.php
@@ -5,36 +5,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Document</title>
-
-    <style>
-        .logo {
-            width: 90px;
-            height: 60px;
-            margin-left: 70px;
-        }
-        .navbar-nav .nav-item + .nav-item {
-            margin-left: 20px; 
-        }
-        .navbar-nav .nav-item  {
-            margin-left: 30px; 
-        }
-        .search_bar {
-            position: absolute;
-            right: 0;
-            margin-right: 20px;
-        }
-    </style>
-</head>
-<body>
+    <body>
     <nav class="navbar navbar-expand bg-body-secondary p-1">
         <div class=" m-2">
-        <img src="./Img_logo/Logo.png" class="logo" alt="">
+            <div class="shine">ITshoeStore</div>
         </div>
         <div class="d-flex">
-            <div class="product collapse navbar-collapse m-0" id="navbarSupportedContent">
+            <div class="product collapse navbar-collapse mt-2" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0  ">
                     <li  class="nav-item dropdown ">
                         <div id="profileNameDisplay" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
